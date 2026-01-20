62e3c6abe4b32d66bce576b67b1ee4abcc42bc06
djumanov <djumanovdev@gmail.com>
2026-01-20 09:14:18 +0500

    models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+
+    class StatusChoices(models.TextChoices):
+        TODO = 'todo', 'To Do'
+        DOING = 'doing', 'Doing'
+        DONE = 'done', 'Done'
+
+    class PriorityChoices(models.TextChoices):
+        LOW = 'low', 'Low'
+        MEDIUM = 'meduim', 'Meduim'
+        HIGH = 'high', 'High'
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    due_date = models.DateTimeField(blank=True, null=True)
+    attechment = models.FileField(blank=True, null=True, upload_to='attechments/')
+    status = models.CharField(
+        max_length=5,
+        choices=StatusChoices.choices,
+        default=StatusChoices.TODO,
+    )
+    priority = models.CharField(
+        max_length=6,
+        choices=PriorityChoices.choices,
+        default=PriorityChoices.LOW,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def __repr__(self):
+        return self.title
