4dc2b0eb0447f2b5ea545d29e2b05e823e99c7ca
djumanov <djumanovdev@gmail.com>
2026-01-24 09:57:19 +0500

    action`

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,5 +1,7 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.utils.html import format_html
+from django.http import HttpRequest
+from django.db.models import QuerySet
 
 from .models import Task, Attechment, SubTask, Category
 
@@ -46,6 +48,24 @@ class CategoryModelAdmin(admin.ModelAdmin):
     list_display_links = ('name', 'colored_slug', 'display_description', 'display_image')
     list_filter = (IsActiveFilter, )
     search_fields = ('name', 'description')
+    fieldsets = [
+        (
+            'Main Information',
+            {
+                "fields": ["name", "is_active"],
+            },
+        ),
+        (
+            "Advanced Options",
+            {
+                "classes": ["collapse"],
+                "fields": ["image", "description"],
+            },
+        ),
+    ]
+    actions = ('mark_as_active', 'mark_as_inactive')
+    list_per_page = 10
+    ordering = ('is_active',)
 
     @admin.display(description='Description')
     def display_description(self, obj):
@@ -67,6 +87,24 @@ class CategoryModelAdmin(admin.ModelAdmin):
             )
         else:
             return self.empty_value_display
+
+    @admin.action(description='Mark as active action')
+    def mark_as_active(self, request: HttpRequest, queryset: QuerySet):
+        updated = queryset.update(is_active=True)
+        self.message_user(
+            request,
+            f"{updated} ta obyekt aktiv qilindi",
+            messages.SUCCESS
+        )
+
+    @admin.action(description='Mark as inactive action')
+    def mark_as_inactive(self, request: HttpRequest, queryset: QuerySet):
+        updated = queryset.update(is_active=False)
+        self.message_user(
+            request,
+            f"{updated} ta obyekt deaktiv qilindi",
+            messages.SUCCESS
+        )
 
 
 @admin.register(Task)
